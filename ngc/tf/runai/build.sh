442e37558a13110efe932d5459a0990db883abf1
chuanli11 <cl.chuanli@gmail.com>
2022-05-01 10:56:21 -0700

    update ngc-tf-runai build with ngc_image_tag=22.04-tf1-py3 --build-arg cuda=11.6

--- ngc/tf/runai/build.sh
+++ ngc/tf/runai/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t chuanli11/ngc-tf-runai:latest --build-arg ngc_image_tag=21.05-tf1-py3 --build-arg cuda=11.3 .
+docker build -f Dockerfile -t chuanli11/ngc-tf-runai:latest --build-arg ngc_image_tag=22.04-tf1-py3 --build-arg cuda=11.6 .

d9cdda88b33a700d017a894d3265b69ad3e59e6a
chuanli11 <cl.chuanli@gmail.com>
2022-05-01 10:38:40 -0700

    Add first build for ngc-tf-runai

---
+++ ngc/tf/runai/README.md
@@ -0,0 +1,8 @@
+# Build TF image for RUNAI distributed training
+
+
+## Build and Release
+
+```
+./release.sh
+```
---
+++ ngc/tf/runai/release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=chuanli11
+# image name
+IMAGE=ngc-tf-runai
+
+
+# ensure we're up to date
+git pull
+
+# bump version
+docker run --rm -v "$PWD":/app treeder/bump patch
+version=`cat VERSION`
+echo "version: $version"
+
+
+# build it
+./build.sh
+
+# tag the latest build
+git add -A
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
+git push --tags
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+# push it
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version

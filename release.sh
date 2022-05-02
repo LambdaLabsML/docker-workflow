fd25a9167ce829579dde3f6105ca451f30569322
chuanli11 <cl.chuanli@gmail.com>
2022-05-01 18:39:37 -0700

    release script is shared across different images

--- release.sh
+++ release.sh
@@ -4,21 +4,23 @@
 # SET THE FOLLOWING VARIABLES
 # docker hub username
 USERNAME=chuanli11
+# build script
+BUILD_SCRIPT=horovod/examples # ngc-tf/runai
 # image name
-IMAGE=ngc-tf-runai
+IMAGE=`echo ${BUILD_SCRIPT} | tr / -` # select from ngc-tf-runai, horovod-examples
 
+echo $IMAGE
 
 # ensure we're up to date
 git pull
 
 # bump version
-docker run --rm -v "$PWD":/app treeder/bump patch
+docker run --rm -v "$PWD":/app treeder/bump --filename VERSION patch
 version=`cat VERSION`
 echo "version: $version"
 
-
 # build it
-./build.sh
+./${BUILD_SCRIPT}.sh
 
 # tag the latest build
 git add -A
@@ -27,6 +29,7 @@ git tag -a "$version" -m "version $version"
 git push
 git push --tags
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
